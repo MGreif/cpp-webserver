61b71038ac418002ea8a12d31f5bd67c143d23c4
Mika Greif <mika@greif.me>
2024-06-29 18:57:32 +0200

    Small adjustments. Cleanup g++ warnings

--- main.cpp
+++ main.cpp
@@ -22,8 +22,8 @@
 
 using namespace std;
 
-
-enum HTTPMethod {
+enum HTTPMethod
+{
     GET,
     POST,
     PATCH,
@@ -36,47 +36,66 @@ enum HTTPMethod {
 int server_socket;
 int client_socket;
 
-void interrupt_event(int i) {
-    cout << "Caught signal: " << i << endl; 
+void interrupt_event(int i)
+{
+    cout << "Caught signal: " << i << endl;
     int ok = close(server_socket);
     shutdown(server_socket, SHUT_RDWR);
-    if (ok == -1) {
+    if (ok == -1)
+    {
         cout << "Failed to close socket: " << server_socket;
-    } else {
+    }
+    else
+    {
         cout << "Closed socket: " << server_socket;
     }
 
     ok = close(client_socket);
     shutdown(client_socket, SHUT_RDWR);
-    if (ok == -1) {
+    if (ok == -1)
+    {
         cout << "Failed to close client socket: " << client_socket;
-    } else {
+    }
+    else
+    {
         cout << "Closed client socket: " << client_socket;
     }
 
-    exit(1); 
+    exit(1);
 }
 
+int main(int argc, char **argv)
+{
 
-int main(int argc, char** argv) {
-    
     int soc_fd = socket(AF_INET, SOCK_STREAM, 0);
     server_socket = soc_fd;
-    if (soc_fd == -1) {
+    if (soc_fd == -1)
+    {
         cout << "Failed to create socket" << endl;
         return -1;
     }
 
-    int networkPort = 3002;
+    if (argc == 1)
+    {
+        printf("usage: %s <port>", argv[0]);
+        exit(1);
+    }
+
+    int networkPort;
+    int ok = sscanf(argv[1], "%d", &networkPort);
+    if (ok != 1)
+    {
+        printf("usage: %s <port:int>", argv[0]);
+    }
 
     sockaddr_in add;
     add.sin_family = AF_INET;
     add.sin_port = htons(networkPort);
     inet_aton("0.0.0.0", &add.sin_addr);
 
-
-    int ok = bind(soc_fd, (sockaddr*)&add, sizeof(add));
-    if (ok  == -1) {
+    ok = bind(soc_fd, (sockaddr *)&add, sizeof(add));
+    if (ok == -1)
+    {
         cout << "Failed to bind socket" << endl;
         printf("%s \n", strerror(errno));
         return -1;
@@ -84,56 +103,60 @@ int main(int argc, char** argv) {
     cout << "Successfully bound socket" << endl;
 
     ok = listen(soc_fd, 10);
-    if (ok  == -1) {
+    if (ok == -1)
+    {
         cout << "Failed to listen" << endl;
         printf("%s \n", strerror(errno));
         return -1;
     }
 
-    cout << "Successfully listening socket on port: " << networkPort  << endl;
-
+    cout << "Successfully listening socket on port: " << networkPort << endl;
 
     sockaddr client;
+    socklen_t *client_len = (socklen_t *)&client;
     int socket_fd;
-    char socket_buffer[512];
-    char input_buffer[256];
 
     signal(SIGINT, interrupt_event);
 
-    while((socket_fd = accept(soc_fd, &client, (socklen_t*)&client))) {
+    while ((socket_fd = accept(soc_fd, &client, client_len)))
+    {
         cout << "Connection accepted ..." << endl;
         client_socket = socket_fd;
 
-        RequestReceiver * receiver;
+        RequestReceiver *receiver;
+        // TODO: Inject request pointer into RequestReceiver. Let the receiver fill the request.
+        // Also maybe adjust this code, to use only one request receiver
+        while ((receiver = RequestReceiver::receive(new RequestReceiver(), client_socket)) != NULL)
+        {
 
-        while((receiver = RequestReceiver::receive(new RequestReceiver(), client_socket)) != NULL) {     
-
-            char  * fullRequest = receiver->getFullRequestString();
-            if (fullRequest == nullptr) {
+            char *fullRequest = receiver->getFullRequestString();
+            if (fullRequest == nullptr)
+            {
                 break;
-            } else {
+            }
+            else
+            {
             }
 
-            Request * request = new Request(receiver->id);
+            Request *request = new Request(receiver->id);
             Request::buildFromString(request, fullRequest);
 
-
-            printf("%d: Method: %s \n", request->id, request->getMethod());
-            printf("%d: Path: %s \n", request->id, request->getPath());
-            printf("%d: Protocol: %s \n", request->id, request->getProtocol());
+            printf("%ld: Method: %s \n", request->id, request->getMethod());
+            printf("%ld: Path: %s \n", request->id, request->getPath());
+            printf("%ld: Protocol: %s \n", request->id, request->getProtocol());
             HeaderMap headers = *request->getHeaders();
             int headerCount = headers.headerCount;
-            printf("%d: Header count: %d \n", request->id, headerCount);
+            printf("%ld: Header count: %d \n", request->id, headerCount);
 
-            for (int i = 0; i < headerCount; i ++) {
-                char headerTemp[REQUEST_HEADER_KEY_LENGTH + REQUEST_HEADER_VALUE_LENGTH + 2] = { '\0'};
+            for (int i = 0; i < headerCount; i++)
+            {
+                char headerTemp[REQUEST_HEADER_KEY_LENGTH + REQUEST_HEADER_VALUE_LENGTH + 2] = {'\0'};
                 ((Header *)headers.headers[i])->toString(headerTemp, REQUEST_HEADER_KEY_LENGTH + REQUEST_HEADER_VALUE_LENGTH + 2);
-                printf("%d: Header -> %s \n", request->id, headerTemp);
-                
+                printf("%ld: Header -> %s \n", request->id, headerTemp);
             }
 
             ResponseTransmitter rt = ResponseTransmitter(socket_fd);
-            Response * r = new HTMLResponse();
+            Response *r = new HTMLResponse();
 
             r->statusCode = 200;
             char hK[] = "TEST";
@@ -142,19 +165,13 @@ int main(int argc, char** argv) {
             char body[512] = "<em>It Works ...</em>";
             r->headers->addHeader(&testHeader);
             strncpy(r->body, body, sizeof(body));
-            
-            
-            
+
             rt.transmit(r);
-            delete r;
             close(client_socket);
-
-
 
             // Cleanup
             delete request;
             receiver->destroy();
-            
         }
     }
     return 0;
--- webserver.cpp
+++ webserver.cpp
@@ -14,135 +14,144 @@
 
 using namespace std;
 
-
 // Header
-void Header::toString(char * out, int size) {
+void Header::toString(char *out, int size)
+{
     char concatenated[REQUEST_HEADER_KEY_LENGTH + REQUEST_HEADER_VALUE_LENGTH + 2] = {'\0'};
     int nameLength = strlen(name);
     strncpy(concatenated, name, nameLength);
-    strncpy(concatenated+nameLength, ": ", 2);
-    strncpy(concatenated+nameLength+2, value, strlen(value));
+    strncpy(concatenated + nameLength, ": ", 2);
+    strncpy(concatenated + nameLength + 2, value, strlen(value));
     strncpy(out, concatenated, size);
 }
 
-
 // HeaderMap
-void HeaderMap::addHeader(Header * h) {
+void HeaderMap::addHeader(Header *h)
+{
     int lastHeaderIndex = this->headerCount;
-    this->headers[lastHeaderIndex] = (char*)h;
+    this->headers[lastHeaderIndex] = (char *)h;
     headerCount++;
 }
 
-void HeaderMap::toString(char * out, int * outLength, int size) {
+void HeaderMap::toString(char *out, int *outLength, int size)
+{
     int startOffset = 0;
     int headerMapLength = headerCount * (REQUEST_HEADER_KEY_LENGTH + REQUEST_HEADER_VALUE_LENGTH + 2 + 1); // Headercount * (stringified header length + \n )
-    char * tempBuffer = new char[headerMapLength]; 
-    for (int i =0; i < headerCount; i++) {
-        Header * header = (Header *) headers[i];
-        char headerBuffer[REQUEST_HEADER_KEY_LENGTH + REQUEST_HEADER_VALUE_LENGTH + 2] = { '\0' };
+    char *tempBuffer = new char[headerMapLength];
+    for (int i = 0; i < headerCount; i++)
+    {
+        Header *header = (Header *)headers[i];
+        char headerBuffer[REQUEST_HEADER_KEY_LENGTH + REQUEST_HEADER_VALUE_LENGTH + 2] = {'\0'};
         header->toString(headerBuffer, REQUEST_HEADER_KEY_LENGTH + REQUEST_HEADER_VALUE_LENGTH + 2);
         int currentHeaderLength = strlen(headerBuffer);
-        strncpy(tempBuffer+startOffset, headerBuffer, currentHeaderLength);
-        strcpy(tempBuffer+startOffset+currentHeaderLength, "\n");
+        strncpy(tempBuffer + startOffset, headerBuffer, currentHeaderLength);
+        strcpy(tempBuffer + startOffset + currentHeaderLength, "\n");
         startOffset += currentHeaderLength;
         startOffset += 1;
     }
-
 
     *outLength = startOffset;
     strncpy(out, tempBuffer, size);
 }
 
-void HeaderMap::destroy() {
+void HeaderMap::destroy()
+{
     cout << "Destroying HeaderMap" << endl;
-    for (int i =0; i< this->headerCount;i++) {
+    cout << "Destroying headers: " << this->headerCount << endl;
+    for (int i = 0; i < this->headerCount; i++)
+    {
         cout << "Destroying Header " << i << endl;
         delete this->headers[i];
     }
+    cout << "Destroyed headers" << endl;
 }
-
 
 // Request
 
-void Request::buildHeaderMapFromString() {
-    if (raw == nullptr) {
+void Request::buildHeaderMapFromString()
+{
+    if (raw == nullptr)
+    {
         return;
     }
 
     char requestHeaderPart[rawLength];
     memset(requestHeaderPart, '\0', sizeof(requestHeaderPart)); // Zero out requestHeaderPart
-    strncpy(requestHeaderPart, raw, rawLength); // This is currently sufficient, as the raw string only contains the headers. THIS WILL BE CHANGED SOON
-    char * dataStartPointer = requestHeaderPart;
-    char * ptr1;
+    strncpy(requestHeaderPart, raw, rawLength);                 // This is currently sufficient, as the raw string only contains the headers. THIS WILL BE CHANGED SOON
+    char *dataStartPointer = requestHeaderPart;
+    char *ptr1;
     bool skippedMethodHeaderLine = false;
-    while((ptr1 = strchr(dataStartPointer, '\n'))!=NULL) {
-        if (!skippedMethodHeaderLine) {
+    while ((ptr1 = strchr(dataStartPointer, '\n')) != NULL)
+    {
+        if (!skippedMethodHeaderLine)
+        {
             // Skipping first line, as it contains no header, but the METHOD, PATH and PROTOCOL
             skippedMethodHeaderLine = true;
-            strcpy(dataStartPointer, ptr1+1);
+            strcpy(dataStartPointer, ptr1 + 1);
             continue;
         }
-        *ptr1 = '\0'; // \n
-        *(ptr1-1) = '\0'; // \r
-        char currentHeader[REQUEST_HEADER_KEY_LENGTH + REQUEST_HEADER_VALUE_LENGTH + 3] = { '\0' };
+        *ptr1 = '\0';       // \n
+        *(ptr1 - 1) = '\0'; // \r
+        char currentHeader[REQUEST_HEADER_KEY_LENGTH + REQUEST_HEADER_VALUE_LENGTH + 3] = {'\0'};
         strncpy(currentHeader, dataStartPointer, strlen(dataStartPointer));
 
-        char * leftOver = ptr1+1;
+        char *leftOver = ptr1 + 1;
         dataStartPointer = leftOver;
 
         char key[REQUEST_HEADER_KEY_LENGTH];
         char value[REQUEST_HEADER_VALUE_LENGTH];
-        char * delimeterPtr = strchr(currentHeader, ':');
-        if (delimeterPtr == nullptr) {
+        char *delimeterPtr = strchr(currentHeader, ':');
+        if (delimeterPtr == nullptr)
+        {
             continue;
         }
         *delimeterPtr = '\0';
         strncpy(key, currentHeader, sizeof(key));
-        strncpy(value, delimeterPtr+2, sizeof(value)); // +2 because in HTTP there is a space behind the colon (': ')
-        Header * h = new Header(key, value);
+        strncpy(value, delimeterPtr + 2, sizeof(value)); // +2 because in HTTP there is a space behind the colon (': ')
+        Header *h = new Header(key, value);
         this->headermap->addHeader(h);
-
     }
 }
-void Request::populateMethodPathAndProtocol() {
-    
-    if (this->raw == nullptr) {
+void Request::populateMethodPathAndProtocol()
+{
+
+    if (this->raw == nullptr)
+    {
         return;
     }
     char requestHeaderPart[rawLength];
     memset(requestHeaderPart, '\0', sizeof(requestHeaderPart)); // Zero out requestHeaderPart
-    strncpy(requestHeaderPart, raw, rawLength); // This is currently sufficient, as the raw string only contains the headers. THIS WILL BE CHANGED SOON
+    strncpy(requestHeaderPart, raw, rawLength);                 // This is currently sufficient, as the raw string only contains the headers. THIS WILL BE CHANGED SOON
 
-
-    
-    char * firstNewLine = strchr(requestHeaderPart, '\n');
+    char *firstNewLine = strchr(requestHeaderPart, '\n');
     *firstNewLine = '\0';
 
     char firstLine[rawLength];
     strncpy(firstLine, requestHeaderPart, sizeof(firstLine));
 
     // Parsing method
-    char * afterMethod = strchr(firstLine, '\x20');
+    char *afterMethod = strchr(firstLine, '\x20');
     *afterMethod = '\0';
-    strncpy(this->method, firstLine, sizeof(firstLine));
+    strncpy(this->method, firstLine, sizeof(this->method));
 
     // Parsing path
-    char * afterPath = strchr(afterMethod + 1, '\x20');
+    char *afterPath = strchr(afterMethod + 1, '\x20');
     *afterPath = '\0';
     strncpy(this->path, afterMethod + 1, sizeof(afterMethod));
 
     // Parsing Protocol
-    strncpy(this->protocol, afterPath+1, sizeof(afterPath));
-
-
+    strncpy(this->protocol, afterPath + 1, sizeof(afterPath));
 }
 
-void Request::buildFromString(Request * out, char * rawRequest) {
+void Request::buildFromString(Request *out, char *rawRequest)
+{
     out->setRawRequest(rawRequest);
     out->parseRequest();
 };
-int Request::parseRequest() {
-    if (this->raw == nullptr) {
+int Request::parseRequest()
+{
+    if (this->raw == nullptr)
+    {
         return -1;
     }
     buildHeaderMapFromString();
@@ -153,41 +162,46 @@ int Request::parseRequest() {
 // RequestReceiver
 // We basically need to check for the end of the HTTP request via the content-length or the transfer-encoding
 
-char * RequestReceiver::getNextChunkPointer() {
-    if (this->chunkCounter == maxChunks - 1) {
+char *RequestReceiver::getNextChunkPointer()
+{
+    if (this->chunkCounter == maxChunks - 1)
+    {
         cout << this->id << ": Max chunks reached ..." << endl;
         return nullptr;
     }
-    char * newChunk = new char[chunkSize];
+    char *newChunk = new char[chunkSize];
     this->chunkPointer[chunkCounter] = newChunk;
-    printf("%d: Allocated chunk %p at counter %d \n", this->id, newChunk, chunkCounter);
+    printf("%ld: Allocated chunk %p at counter %d \n", this->id, newChunk, chunkCounter);
     chunkCounter++;
     return newChunk;
 }
 
-
 // Just putting current chunks together
-char * RequestReceiver::getCurrentData() {
-    char * allocatedPointer = new char[maxChunks * chunkSize]; // TODO maybe just allocate chunkCounter * chunkSize of chunk is small
-    for (int i = 0; i < chunkCounter; i++) {
+char *RequestReceiver::getCurrentData()
+{
+    char *allocatedPointer = new char[maxChunks * chunkSize]; // TODO maybe just allocate chunkCounter * chunkSize of chunk is small
+    for (int i = 0; i < chunkCounter; i++)
+    {
         strcpy(allocatedPointer + i * chunkSize, chunkPointer[i]);
     }
     return allocatedPointer;
 }
 // Checks if the current chunk contains end of the request. Returns length of request
 // This is still WIP and currently only parses GET Requests
-long RequestReceiver::getRequestEnd() {
+long RequestReceiver::getRequestEnd()
+{
 
     char requestEndPattern[] = "\r\n\r\n";
-    char * currentData = getCurrentData();
-    
-    char * indexOfStringStart = strstr(currentData, requestEndPattern);
-    if (indexOfStringStart == NULL) {
+    char *currentData = getCurrentData();
+
+    char *indexOfStringStart = strstr(currentData, requestEndPattern);
+    if (indexOfStringStart == NULL)
+    {
         return 0;
     }
     int len = strlen(requestEndPattern);
 
-    char * indexOfStringEnd = indexOfStringStart + len;
+    char *indexOfStringEnd = indexOfStringStart + len;
 
     long lengthOfRequest = indexOfStringEnd - currentData;
 
@@ -195,35 +209,40 @@ long RequestReceiver::getRequestEnd() {
     return lengthOfRequest;
 }
 
-//Allocate chunks in heap and fill chunks with request data
-RequestReceiver * RequestReceiver::receive(RequestReceiver * r, int socket) {
+// Allocate chunks in heap and fill chunks with request data
+RequestReceiver *RequestReceiver::receive(RequestReceiver *r, int socket)
+{
     int ok;
 
     char tempBuffer[chunkSize];
-    while((ok = recv(socket, tempBuffer, chunkSize, 0))) {
-        int currentChunk = r->chunkCounter;
-        char * nextChunkPointer = r->getNextChunkPointer();
-        if (nextChunkPointer == nullptr) {
-            cout << r->id << ": Failed to get next chunk pointer. Returning ..." << endl;
-            return r;
-        } 
-        if (ok == -1) {
+    while ((ok = recv(socket, tempBuffer, chunkSize, 0)))
+    {
+        if (ok == -1)
+        {
             cout << r->id << ": Error receiving" << endl;
-            printf("%s \n", strerror(errno));
+            printf("Error: %s \n", strerror(errno));
             return r;
         }
-        
+        cout << "Read: " << ok << " Bytes" << endl;
+        int currentChunk = r->chunkCounter;
+        char *nextChunkPointer = r->getNextChunkPointer();
+        if (nextChunkPointer == nullptr)
+        {
+            cout << r->id << ": Failed to get next chunk pointer. Returning ..." << endl;
+            return r;
+        }
 
         memcpy(nextChunkPointer, tempBuffer, chunkSize);
         memset(tempBuffer, '\0', chunkSize);
 
-        printf("%d: Working at chunkindex: %d \n", r->id, currentChunk);
+        printf("%ld: Working at chunkindex: %d \n", r->id, currentChunk);
 
         long requestEndOffset = r->getRequestEnd();
-        if (requestEndOffset != 0) {
-            printf("%d: End of request detected in chunk %d... \n", r->id, currentChunk);
+        if (requestEndOffset != 0)
+        {
+            printf("%ld: End of request detected in chunk %d... \n", r->id, currentChunk);
             long requestEndOffsetInCurrentChunk = requestEndOffset % r->chunkSize;
-            printf("%d: requestEndOffset in current chunk: %d \n", r->id, requestEndOffsetInCurrentChunk);
+            printf("%ld: requestEndOffset in current chunk: %ld \n", r->id, requestEndOffsetInCurrentChunk);
             *(nextChunkPointer + requestEndOffsetInCurrentChunk) = '\0';
             r->requestFinished = true;
             r->requestEndOffset = requestEndOffset;
@@ -231,24 +250,28 @@ RequestReceiver * RequestReceiver::receive(RequestReceiver * r, int socket) {
         }
     }
     return r;
-
 };
-char * RequestReceiver::getFullRequestString() {
-    if (!this->requestFinished) {
+char *RequestReceiver::getFullRequestString()
+{
+    if (!this->requestFinished)
+    {
         return nullptr;
     }
-    if (this->fullRequestPointer != nullptr) {
+    if (this->fullRequestPointer != nullptr)
+    {
         return this->fullRequestPointer;
     }
-    char * allocatedPointer = getCurrentData();
+    char *allocatedPointer = getCurrentData();
 
     this->fullRequestPointer = allocatedPointer;
     return fullRequestPointer;
 };
 
 // Cleanup all allocated memory
-void RequestReceiver::destroy() {
-    for (int i = 0; i < chunkCounter; i++) {
+void RequestReceiver::destroy()
+{
+    for (int i = 0; i < chunkCounter; i++)
+    {
         cout << this->id << ": Deleting chunk: " << i << endl;
         delete[] chunkPointer[i];
     }
@@ -257,111 +280,110 @@ void RequestReceiver::destroy() {
     delete[] fullRequestPointer;
 }
 
+void getStatusCodeMapping(char *out, int size, int statusCode)
+{
 
-void getStatusCodeMapping(char * out, int size, int statusCode) {
-    
     switch (statusCode)
     {
     case 200:
         strncpy(out, "OK", size);
         break;
-    
+
     // TODO FILL OTHER STATUS CODES HERE
     default:
         strncpy(out, "OK", size);
         break;
-    }
-    ;
+    };
 }
 
 // Response
-char * Response::serialize() {
-    char * responseBuffer = new char[RESPONSE_BUFFER_SIZE];
-    char * stringPointer = responseBuffer;
-    
+char *Response::serialize()
+{
+    char *responseBuffer = new char[RESPONSE_BUFFER_SIZE];
+    char *stringPointer = responseBuffer;
+
     // PROTOCOL
     strncpy(stringPointer, this->protocol, strlen(this->protocol));
-    stringPointer+=strlen(this->protocol);
+    stringPointer += strlen(this->protocol);
 
     strncpy(stringPointer, " ", 1);
-    stringPointer+=1;
+    stringPointer += 1;
 
     // STATUS CODE INT
     char number[10];
     sprintf(number, "%d", this->statusCode);
 
     strncpy(stringPointer, number, strlen(number));
-    stringPointer+=strlen(number);
+    stringPointer += strlen(number);
 
     strncpy(stringPointer, " ", 1);
-    stringPointer+=1;
+    stringPointer += 1;
 
     // STATUS CODE MAPPING
     char statusCodeMapping[10];
     getStatusCodeMapping(statusCodeMapping, sizeof(statusCodeMapping), this->statusCode);
 
-
     strncpy(stringPointer, statusCodeMapping, strlen(statusCodeMapping));
-    stringPointer+=strlen(statusCodeMapping);
+    stringPointer += strlen(statusCodeMapping);
 
     strncpy(stringPointer, "\r\n", 2);
-    stringPointer+=2;
+    stringPointer += 2;
 
     // HEADERS
-    if (this->headers != nullptr) {
+    if (this->headers != nullptr)
+    {
         int headerLength;
         this->headers->toString(stringPointer, &headerLength, RESPONSE_BUFFER_SIZE);
 
-        stringPointer+=headerLength;
-
+        stringPointer += headerLength;
     }
 
     // BODY
     // TODO ADD BODY
     strncpy(stringPointer, "\r\n\r\n", 4);
-    stringPointer+=4;
+    stringPointer += 4;
 
     strncpy(stringPointer, this->body, strlen(this->body));
-    stringPointer+=17;
+    stringPointer += 17;
 
-    if (this->responseBufferPtr != NULL) {
+    if (this->responseBufferPtr != NULL)
+    {
         delete[] this->responseBufferPtr;
     }
 
     this->responseBufferPtr = responseBuffer;
 
     return responseBuffer;
-    
 }
-    Response::~Response() {
+Response::~Response()
+{
     cout << "Destroy Response" << endl;
     delete this->headers;
     delete[] this->responseBufferPtr;
 }
 
-
 // HTMLResponse
 
-void HTMLResponse::injectDefaultHeaders(HeaderMap* hm) {
+void HTMLResponse::injectDefaultHeaders(HeaderMap *hm)
+{
     char ctKey[] = "Content-Type";
     char ctValue[] = "text/html";
     char cKey[] = "Connection";
     char cValue[] = "close";
-    Header * contentType = new Header(ctKey, ctValue);
-    Header * connection = new Header(cKey, cValue);
+    Header *contentType = new Header(ctKey, ctValue);
+    Header *connection = new Header(cKey, cValue);
     hm->addHeader(contentType);
     hm->addHeader(connection);
 }
 
-
-
 // ResponseTransmitter
 
-
-ResponseTransmitter::ResponseTransmitter(int socket_fd) {
+ResponseTransmitter::ResponseTransmitter(int socket_fd)
+{
     this->client_socket = socket_fd;
 }
-void ResponseTransmitter::transmit(Response * r) {
-    char * responseBody = r->serialize();
+void ResponseTransmitter::transmit(Response *r)
+{
+    char *responseBody = r->serialize();
     send(this->client_socket, responseBody, strlen(responseBody), 0);
 }
